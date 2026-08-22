7ebae47e1ab33913b9e9fb88836939688b09000b
Uday Kumar <sudaykumar2608@gmail.com>
2026-08-22 11:06:00 +0530

    fix: enable TCP keepalives to prevent Aiven from dropping idle DB connections

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -11,6 +11,8 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 
   ssl: {
     rejectUnauthorized: false
